40ce69ea9447d64fa891db13db00aadded5cd266
Engr-Awais-Ahmad <awaisahmad2b@gmail.com>
2025-09-09 17:55:51 +0500

    MVVM Structure

---
+++ lib/core/constant/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color mainColor = Color(0xffD6B99E);
+const Color redishColor = Color(0xffE78377);
+const Color blackColor = Color(0xff000000);
+const Color whiteColor = Color(0xffFFFFFF);
+const Color greyColor = Color(0xff888888);
